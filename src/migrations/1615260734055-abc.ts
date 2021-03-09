f5faef8f543976e44df864b93a1014f5ce963719
LuxuryFi <anhit.luxuryfi@gmail.com>
2021-03-09 10:33:18 +0700

    Update from Laptop

--- src/migrations/1615260734055-abc.ts
+++ src/migrations/1615260734055-abc.ts
@@ -1,16 +1,16 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class abc1615227263101 implements MigrationInterface {
-    name = 'abc1615227263101'
+export class abc1615260734055 implements MigrationInterface {
+    name = 'abc1615260734055'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `topic` ADD `topic_code` varchar(255) NOT NULL");
         await queryRunner.query("ALTER TABLE `topic` ADD `semester` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `topic` ADD `description` varchar(255) NOT NULL");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `topic` DROP COLUMN `description`");
         await queryRunner.query("ALTER TABLE `topic` DROP COLUMN `semester`");
-        await queryRunner.query("ALTER TABLE `topic` DROP COLUMN `topic_code`");
     }
 
 }
